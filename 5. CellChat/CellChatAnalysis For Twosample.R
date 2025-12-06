9a1f7b7bebf9a29e1f8c987fcca531fea2177f2d
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-06 21:45:43 +0800

    Update CellChatAnalysis For Twosample.R

--- 5. CellChat/CellChatAnalysis For Twosample.R
+++ 5. CellChat/CellChatAnalysis For Twosample.R
@@ -1,1 +1,32 @@
+# ============================== Prepare Environment ===============================
+Sys.setenv(LANGUAGE = "en")
+options(stringsAsFactors = FALSE)
+rm(list=ls());gc()
+setwd("workspace")
+getwd()
+library(qs)
+library(CellChat)
+library(dplyr)
+library(Seurat)
+library(ggpubr)
+library(cowplot)
+library(ggplot2)
+library(ggpubr)
+library(harmony)
+library(patchwork)
+library(RColorBrewer)
+library(SCP)
+
+set.seed(1234)
+# 查看工作路径下的文件
+list.files()
+dir.create("../03.Output/")
+
+
+# ================================================================ Load scData for cellchat ==========================================================
+seurat_obj <- qread("seurat_obj.qs")
+ncol(seurat_obj)
+Idents(seurat_obj)
+DimPlot(seurat_obj, pt.size = 0.8,group.by = "celltype_major",label = T)
+table(seurat_obj@meta.data$celltype_major)
 
