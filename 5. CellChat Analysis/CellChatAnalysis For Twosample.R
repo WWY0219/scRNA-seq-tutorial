c98d731ca084c0c50609f431d2af62ee35145963
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-07 17:37:13 +0800

    Update CellChatAnalysis For Twosample.R

--- 5. CellChat Analysis/CellChatAnalysis For Twosample.R
+++ 5. CellChat Analysis/CellChatAnalysis For Twosample.R
@@ -15,6 +15,7 @@ library(ggpubr)
 library(harmony)
 library(patchwork)
 library(RColorBrewer)
+library(ComplexHeatmap)
 library(SCP)
 
 set.seed(1234)
@@ -55,7 +56,6 @@ identical(rownames(meta),colnames(data.input))
 cellchat <- createCellChat(object = data.input, meta = meta, group.by = "labels")
 CellChatDB <- CellChatDB.human  
 CellChatDB.use <- subsetDB(CellChatDB)
-#CellChatDB.use <- CellChatDB 
 cellchat@DB <- CellChatDB.use
 cellchat <- subsetData(cellchat) # This step is necessary even if using the whole databa
 future::plan("multisession", workers = 1) # do parallel
@@ -75,8 +75,8 @@ object.list <- list(left=cellchat_left, right=cellchat_right)
 cellchat <- mergeCellChat(object.list, add.names = names(object.list))
 cellchat
 
-# ================================================================ Load scData for cellchat ==========================================================
-## ------------
+# ================================================================ CellChat Visulization ==========================================================
+## ------------柱状图---------------
 gg1 <- compareInteractions(cellchat, show.legend = F, 
                            group = c(1,2))
 gg2 <- compareInteractions(cellchat, show.legend = F, 
@@ -87,14 +87,138 @@ gg3 <- rankNet(cellchat, mode = "comparison", stacked = T, do.stat = TRUE)
 gg4 <- rankNet(cellchat, mode = "comparison", stacked = F, do.stat = TRUE)
 gg3 + gg4
 
+## ------------circle---------------
+netVisual_diffInteraction(cellchat, weight.scale = T)
+netVisual_diffInteraction(cellchat, weight.scale = T, measure = "weight")
+
+## ------------heatmap---------------
+gg1 <- netVisual_heatmap(cellchat)
+gg1
+gg2 <- netVisual_heatmap(cellchat, measure = "weight")
+gg2
+
+weight.max <- getMaxWeight(object.list, attribute = c("idents","count"))
+par(mfrow = c(1,2), xpd=TRUE)
+for (i in 1:length(object.list)) {
+  netVisual_circle(object.list[[i]]@net$count, weight.scale = T, label.edge= F, edge.weight.max = weight.max[2], edge.width.max = 12, title.name = paste0("Number of interactions - ", names(object.list)[i]))
+}
+
+## ------------violin------------------
+cellchat@meta$datasets = factor(cellchat@meta$datasets, levels = c("left", "right")) # set factor level
+plotGeneExpression(cellchat, signaling = "CXCL", split.by = "datasets", colors.ggplot = T, type = "violin")
+
+## ------------violin------------------
+num.link <- sapply(object.list, function(x) {rowSums(x@net$count) + colSums(x@net$count)-diag(x@net$count)})
+weight.MinMax <- c(min(num.link), max(num.link)) # control the dot size in the different datasets
+gg <- list()
+for (i in 1:length(object.list)) {
+  gg[[i]] <- netAnalysis_signalingRole_scatter(object.list[[i]], title = names(object.list)[i], weight.MinMax = weight.MinMax)
+}
+
+patchwork::wrap_plots(plots = gg)
+
+## 可视化从left样本到right样本的发出与接收信号差异变化
+gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "T/NK", signaling.exclude = "MIF")
+gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "Tumor", signaling.exclude = c("MIF"))
+patchwork::wrap_plots(plots = list(gg1,gg2))
+
+## -------------------------------
+i = 1
+# combining all the identified signaling pathways from different datasets 
+pathway.union <- union(object.list[[i]]@netP$pathways, object.list[[i+1]]@netP$pathways)
+ht1 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "outgoing", signaling = pathway.union, title = names(object.list)[i], width = 5, height = 6)
+ht2 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "outgoing", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 6)
+ht1
+ht2
+
+## -----------------------
+netVisual_bubble(cellchat, sources.use = 4, targets.use = c(5:7),  comparison = c(1, 2), angle.x = 45)
+
+gg1 <- netVisual_bubble(cellchat, 
+                        sources.use = 4, 
+                        targets.use = c(1),  
+                        comparison = c(1, 2), 
+                        max.dataset = 2, 
+                        title.name = "Increased signaling in Left", angle.x = 45, 
+                        remove.isolate = T)
+gg2 <- netVisual_bubble(cellchat, 
+                        sources.use = 4, 
+                        targets.use = c(1),  
+                        comparison = c(1, 2), 
+                        max.dataset = 1, 
+                        title.name = "Decreased signaling in right", angle.x = 45, 
+                        remove.isolate = T)
+gg1 + gg2
 
 
+## ------------------Chord diagram-------------------
+par(mfrow = c(1,2), xpd=TRUE, mar = c(1,1,2,1))
+netVisual_chord_gene(object.list[[2]], 
+                     sources.use = 4, 
+                     targets.use = c(1), 
+                     slot.name = 'net', 
+                     net = net.up, 
+                     lab.cex = 0.8, small.gap = 3.5, 
+                     title.name = paste0("Up-regulated signaling in ", names(object.list)[2]))
+netVisual_chord_gene(object.list[[1]], 
+                     sources.use = 4, 
+                     targets.use = c(1), 
+                     slot.name = 'net', 
+                     net = net.down, 
+                     lab.cex = 0.8, small.gap = 3.5, 
+                     title.name = paste0("Down-regulated signaling in ", names(object.list)[2]))
 
+## ----------------------词云图----------------------
+par(mfrow = c(1,2), xpd=TRUE, mar = c(1,1,2,1))
+library(wordcloud)
+computeEnrichmentScore(net.down, species = 'human', variable.both = TRUE)
+computeEnrichmentScore(net.up, species = 'human', variable.both = TRUE)
 
+## --------------------------------------------------
+pathways.show <- c("CXCL") 
+weight.max <- getMaxWeight(object.list, slot.name = c("netP"), attribute = pathways.show) # control the edge weights across different datasets
+par(mfrow = c(1,2), xpd=TRUE)
+for (i in 1:length(object.list)) {
+  netVisual_aggregate(object.list[[i]], signaling = pathways.show, layout = "circle", edge.weight.max = weight.max[1], edge.width.max = 10, signaling.name = paste(pathways.show, names(object.list)[i]))
+}
 
+## --------------------------------------------------
+pathways.show <- c("CXCL") 
+par(mfrow = c(1,2), xpd=TRUE)
+ht <- list()
+for (i in 1:length(object.list)) {
+  ht[[i]] <- netVisual_heatmap(object.list[[i]], signaling = pathways.show, color.heatmap = "Reds",title.name = paste(pathways.show, "signaling ",names(object.list)[i]))
+}
 
+# Do heatmap based on a single object
+ComplexHeatmap::draw(ht[[1]] + ht[[2]], ht_gap = unit(0.5, "cm"))
+ht[[1]]
 
+## -----------------------------Chord diagram
+pathways.show <- c("CXCL") 
+par(mfrow = c(1,2), xpd=TRUE)
+for (i in 1:length(object.list)) {
+  netVisual_aggregate(object.list[[i]], signaling = pathways.show, layout = "chord", signaling.name = paste(pathways.show, names(object.list)[i]))
+}
+# Chord diagram 的另外一种形式
+# group.cellType <- c(rep("FIB", 4), rep("DC", 4), rep("TC", 4)) # grouping cell clusters into fibroblast, DC and TC cells
+# names(group.cellType) <- levels(object.list[[1]]@idents)
+# pathways.show <- c("CXCL") 
+# par(mfrow = c(1,2), xpd=TRUE)
+# for (i in 1:length(object.list)) {
+#   netVisual_chord_cell(object.list[[i]], signaling = pathways.show, group = group.cellType, title.name = paste0(pathways.show, " signaling network - ", names(object.list)[i]))
+# }
 
+par(mfrow = c(1, 3), xpd=TRUE)
+for (i in 1:length(object.list)) {
+  netVisual_chord_gene(object.list[[i]], sources.use = 4, targets.use = c(5:6), lab.cex = 0.5, title.name = paste0("Signaling from Tm - ", names(object.list)[i]))
+}
+
+# compare all the interactions sending from fibroblast to inflamatory immune cells
+# par(mfrow = c(1, 2), xpd=TRUE)
+# for (i in 1:length(object.list)) {
+#   netVisual_chord_gene(object.list[[i]], sources.use = c(1,2, 3, 4), targets.use = c(8,10),  title.name = paste0("Signaling received by Inflam.DC and .TC - ", names(object.list)[i]), legend.pos.x = 10)
+# }
 
 
 
