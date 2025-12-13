2669a187fd65148995071c7cc7790ef105d682b0
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-13 20:44:32 +0800

    Update metadata.R

--- FastCode/metadata.R
+++ FastCode/metadata.R
@@ -8,3 +8,6 @@ smc$group <- recode(smc$group, !!!name_mapping)
 # ====================================批量替换meta.data信息==================================
 seurat_obj$group[seurat_obj$orig.ident == "ne1"] <- "NE"
 seurat_obj$celltype <- stringr::str_replace(seurat_obj$celltype, "^Epthelial$", "Epithelial")
+
+seurat_obj@meta.data$orig.ident <- factor(seurat_obj@meta.data$orig.ident, 
+                                          levels=c("ne1","ne2","ne3", ... )
