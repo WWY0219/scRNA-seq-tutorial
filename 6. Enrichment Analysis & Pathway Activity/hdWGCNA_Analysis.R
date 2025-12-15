adffd52fc59e9606a4d32878f1b089017879144e
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-15 10:35:00 +0800

    Update and rename hdWGCNA.R to hdWGCNA_Analysis.R

--- 6. Enrichment Analysis & Pathway Activity/hdWGCNA_Analysis.R
+++ 6. Enrichment Analysis & Pathway Activity/hdWGCNA_Analysis.R
@@ -341,7 +341,32 @@ HubGeneNetworkPlot(
 
 
 ## -------------------UMAP共表达网络------------------
+seurat_obj <- RunModuleUMAP(
+  seurat_obj,
+  n_hubs = 10,       # number of hub genes to include for the UMAP embedding
+  n_neighbors=15,    # neighbors parameter for UMAP
+  min_dist=0.1       # min distance between points in UMAP space
+)
 
+### get the hub gene UMAP table from the seurat object
+umap_df <- GetModuleUMAP(seurat_obj)
+
+# plot with ggplot
+ggplot(umap_df, aes(x=UMAP1, y=UMAP2)) +
+  geom_point(
+   color=umap_df$color,  # color each point by WGCNA module
+   size=umap_df$kME*2    # size of each point based on intramodular connectivity
+  ) +
+  umap_theme()
+
+ModuleUMAPPlot(
+  seurat_obj,
+  edge.alpha=0.25,
+  sample_edges=TRUE,
+  edge_prop=0.1,        # proportion of edges to sample (20% here)
+  label_hubs=2 ,        # how many hub genes to plot per module?
+  keep_grey_edges=FALSE
+)
 
 
 
