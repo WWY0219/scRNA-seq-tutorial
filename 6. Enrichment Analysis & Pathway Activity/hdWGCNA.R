9212605c2810f43d1236e0486fbddbae0ea8f128
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-15 09:55:55 +0800

    Update hdWGCNA.R

--- 6. Enrichment Analysis & Pathway Activity/hdWGCNA.R
+++ 6. Enrichment Analysis & Pathway Activity/hdWGCNA.R
@@ -90,7 +90,7 @@ seurat_obj <- SetDatExpr(
 )
 
 ## -------------------选择软阈值------------------------
-### "unsigned" ：不考虑相关性的正负，仅用相关性的绝对值（适用于研究基因共表达的强弱，不关注调控方向，示例中使用此类型）
+### "unsigned" ：不考虑相关性的正负，仅用相关性的绝对值（适用于研究基因共表达的强弱，不关注调控方向）
 ### "signed"   ：考虑相关性的正负（正相关为激活，负相关为抑制，适用于研究基因调控的方向性）
 ### "signed hybrid"：强调正相关，弱化负相关（常用作折中方案）
 seurat_obj <- TestSoftPowers(
@@ -118,45 +118,131 @@ seurat_obj <- ConstructNetwork(
         min_power = 3,            # 自动选择软阈值时的最小阈值
         tom_outdir = "TOM",       # TOM矩阵的输出目录
         tom_name = 'Treg',        # TOM矩阵的文件名
-        overwrite_tom = TRUE              # 允许覆盖已存在的同名文件
+        overwrite_tom = TRUE,     # 允许覆盖已存在的同名文件
+        consensus = FALSE,        # 是否构建共识网络（多数据集整合）
+        overwrite_tom = FALSE,    # 是否覆盖已存在的TOM文件
+        blocks = NULL,            # 基因分块（处理大量基因时）
+        maxBlockSize = 30000,     # 每个块的最大基因数
+        randomSeed = 12345,       # 随机种子（保证结果可重复）
+        corType = "pearson",      # 相关性计算方法（"pearson"/"spearman"）
+        consensusQuantile = 0.3,  # 共识网络的分位数
+        networkType = "signed",   # 网络类型（"signed"/"unsigned"/"signed hybrid"）
+        TOMType = "signed",       # TOM矩阵类型
+        TOMDenom = "min",         # TOM分母的计算方式（"min" or "mean")
+        scaleTOMs = TRUE,         # 是否缩放TOM矩阵
+        calibrationQuantile =0.95,# 校准分位数
+        sampleForCalibration=TRUE,# 是否抽样校准TOM
+  sampleForCalibrationFactor=1000,# 校准抽样的因子
+        chunkSize = NULL,         # 分块处理的块大小
+        deepSplit = 4,            # 模块检测的深度分割参数
+        pamStage = FALSE,         # 是否使用PAM优化模块
+        detectCutHeight = 0.995,  # 模块检测的剪切高度
+        minModuleSize = 50,       # 最小模块基因数
+        mergeCutHeight = 0.2,     # 模块合并的剪切高度
+        saveConsensusTOMs = TRUE, # 是否保存共识TOM矩阵
 )
-seurat_obj <- ConstructNetwork(
- 
-  
-       
-         
-  consensus = FALSE,        # 是否构建共识网络（多数据集整合）
-  overwrite_tom = FALSE,    # 是否覆盖已存在的TOM文件
-  wgcna_name = NULL,        # hdWGCNA实验名称
-  blocks = NULL,            # 基因分块（处理大量基因时）
-  maxBlockSize = 30000,     # 每个块的最大基因数
-  randomSeed = 12345,       # 随机种子（保证结果可重复）
-  corType = "pearson",      # 相关性计算方法
-  consensusQuantile = 0.3,  # 共识网络的分位数
-  networkType = "signed",   # 网络类型
-  TOMType = "signed",       # TOM矩阵类型
-  TOMDenom = "min",         # TOM分母的计算方式
-  scaleTOMs = TRUE,         # 是否缩放TOM矩阵
-  calibrationQuantile = 0.95,# 校准分位数
-  sampleForCalibration = TRUE,# 是否抽样校准TOM
-  sampleForCalibrationFactor = 1000,# 校准抽样的因子
-  useDiskCache = TRUE,      # 是否使用磁盘缓存存储中间结果
-  chunkSize = NULL,         # 分块处理的块大小
-  deepSplit = 4,            # 模块检测的深度分割参数
-  pamStage = FALSE,         # 是否使用PAM优化模块
-  detectCutHeight = 0.995,  # 模块检测的剪切高度
-  minModuleSize = 50,       # 最小模块基因数
-  mergeCutHeight = 0.2,     # 模块合并的剪切高度
-  saveConsensusTOMs = TRUE, # 是否保存共识TOM矩阵
-  ...                       # 其他传递给WGCNA的参数
-)
+
 ### 可视化WGCNA树状图
 ### ！！！“灰色”模块由那些未被归入任何共表达模块的基因组成。对于所有下游分析和解释，应忽略灰色模块！！！
-PlotDendrogram(seurat_obj, main='Treg hdWGCNA Dendrogram')
+PlotDendrogram(seurat_obj, main='Celltye_1 hdWGCNA Dendrogram')
 
 ### 检查拓扑重叠矩阵(topoligcal overlap matrix，TOM)
 TOM <- GetTOM(seurat_obj)
 TOM
+
+
+##-------------------计算模块特征基因------------------------
+### 模块特征基因(Module Eigengenes，MEs)是用于总结整个共表达模块基因表达谱的常用指标
+### 模块特征基因是通过在每个模块的基因表达矩阵子集上执行主成分(PCA)分析来计算的
+### 这些PCA矩阵的第一个主成分就是MEs。此外对MEs应用Harmony批量校正，从而得到harmony后的模块特征基因(hMEs）
+### 需要先运行ScaleData，否则harmony会报错
+# seurat_obj <- ScaleData(seurat_obj, features=VariableFeatures(seurat_obj))
+
+### 计算完整单细胞数据集中的所有MEs
+seurat_obj <- ModuleEigengenes(
+        seurat_obj,
+        group.by.vars="orig.ident"
+)
+
+### 协调模特征基因:允许用户对MEs应用Harmony批量校正，生成协调模块特征基因(hMEs)
+hMEs <- GetMEs(seurat_obj)
+
+### 提取未经过批次校正的模块特征基因（ME）
+#MEs <- GetMEs(seurat_obj, harmonized=FALSE)
+
+
+##-------------------计算模块连接性------------------------
+### 在共表达网络分析中，通常希望关注“枢纽基因”，即在每个模块内高度连接的基因。因此，希望确定每个基因的基于特征基因(eigengene)的连接性，也称为kME
+### hdWGCNA提供了ModuleConnectivity 函数，用于在完整的单细胞数据集(而不是metacell数据集)中计算基因的kME值。这个函数本质上是计算基因与模块特征基因之间的成对相关性
+### 虽然可以在整个数据集中计算所有细胞的kME，但建议在之前用于运行ConstructNetwork的细胞类型或分组中计算kME
+
+### 计算基于特征基因的连接性(kME)：关注枢纽基因
+seurat_obj <- ModuleConnectivity(
+        seurat_obj,
+        group.by = 'celltype', 
+        group_name = 'celltype_1')
+
+### 模块重命名
+seurat_obj <- ResetModuleNames(
+        seurat_obj,
+        new_name = "celltype_NEW"
+)
+
+### 绘制每个模块按kME排序的基因
+p <- PlotKMEs(seurat_obj, ncol=4)
+p
+
+
+##-------------------获取模块内部信息------------------------
+### 获取模块内部信息:这一步去除了不需要的灰色模块基因
+modules <- GetModules(seurat_obj) %>% 
+  subset(module != 'grey')
+head(modules[,1:6])
+
+### 得到枢纽基因(可以提取按kME排序的前N个枢纽基因的表格,这里选择了10)
+hub_df <- GetHubGenes(seurat_obj, n_hubs = 10)
+head(hub_df)
+
+### 保存数据
+qsave(seurat_obj, 'hdWGCNA_object.qs')
+
+
+##-------------------计算hub基因siganture得分------------------------
+### 计算每个模块前25个枢纽基因的kME得分
+library(UCell)
+seurat_obj <- ModuleExprScore(
+        seurat_obj,
+        n_genes = 25,
+        method='UCell'                # "Seurat" or "UCell"
+        #wgcna_name = NULL,
+)
+
+
+
+# ============================================ Visulization ============================================
+## -------------------模块特征图------------------
+### 每个模块制作hMEs的特征图
+plot_list <- ModuleFeaturePlot(
+        seurat_obj,
+        features='hMEs', # plot the hMEs
+        order=TRUE       # order so the points with highest hMEs are on top
+)
+wrap_plots(plot_list, ncol=4)
+
+
+## -------------------相同函数绘制hub基因特征得分------------------
+### 每个模块制作hub scores的特征图
+plot_list <- ModuleFeaturePlot(
+        eurat_obj,
+        features='scores', # plot the hub gene scores
+        order='shuffle',   # order so cells are shuffled
+        ucell = TRUE       # depending on Seurat vs UCell for gene scoring
+)
+wrap_plots(plot_list, ncol=4)
+
+
+
+
 
 
 
