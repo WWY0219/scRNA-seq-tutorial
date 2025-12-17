c369f0cb4aef8e0cc53c4fd8e861e5700e1ae85e
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-17 22:04:44 +0800

    Update cellranger.sh

--- 0. Cellranger/cellranger.sh
+++ 0. Cellranger/cellranger.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=wwy_cnvkit
+#SBATCH --error=%x.%j.err
+#SBATCH --output=%x.%j.out
+#SBATCH --partition=cpu1,cpu2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64000
 
+
+cellranger count \
+--id run_pbmc_1k_v3_fastqs \
+--create-bam true \
+--transcriptome refdata-gex-GRCh38-2024-A/ \
+--fastqs pbmc_1k_v3_fastqs/ \
+--nosecondary \
+--localcores 16 \
+--localmem 64
