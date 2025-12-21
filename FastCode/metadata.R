d1da23715944c73a570885af60b21c3304008ac3
WWY <173780817+WWY0219@users.noreply.github.com>
2025-12-21 09:49:55 +0800

    Update metadata.R

--- FastCode/metadata.R
+++ FastCode/metadata.R
@@ -10,4 +10,16 @@ seurat_obj$group[seurat_obj$orig.ident == "ne1"] <- "NE"
 seurat_obj$celltype <- stringr::str_replace(seurat_obj$celltype, "^Epthelial$", "Epithelial")
 
 seurat_obj@meta.data$orig.ident <- factor(seurat_obj@meta.data$orig.ident, 
-                                          levels=c("ne1","ne2","ne3", ... )
+                                          levels=c("ne1","ne2","ne3", ... ))
+
+
+# ============================ Add groupdata into meta.data ===================================
+## identify groupnames
+healthy_samples <- c("Healthy_01", "Healthy_02")  
+tumor_samples   <- c("Tumor_01", "Tumor_02")  
+
+seurat_obj@meta.data$group <- ifelse(
+  seurat_obj@meta.data$orig.ident %in% healthy_samples,         # 对照样本ID
+  "Healthy",                                             
+  "Tumor"                                            
+)
