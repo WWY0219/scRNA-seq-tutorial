28e9d98052d9a405bfd03cf1cbe8db5792126529
Wuyang Wang <173780817+WWY0219@users.noreply.github.com>
2026-01-17 11:41:23 +0800

    Refactor QC.R for improved readability and structure

--- 01. Quality Control/QC.R
+++ 01. Quality Control/QC.R
@@ -1,4 +1,4 @@
-#============= Load environment =================
+#========================================================= Load environment ==============================================================
 Sys.setenv(LANGUAGE = "en")
 options(stringsAsFactors = FALSE)
 rm(list=ls())
@@ -8,7 +8,6 @@ getwd()
 library(qs)
 library(dplyr)
 library(Seurat)
-library(ggpubr)
 library(cowplot)
 library(ggplot2)
 library(harmony)
@@ -16,18 +15,14 @@ library(patchwork)
 library(RColorBrewer)
 library(DoubletFinder)
 library(tidyverse)
-library(ggsci)
-library(pheatmap)
-library(scCustomize)
 set.seed(1234)
 dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 
-
-#==================================Load Running script=============================
+#=========================================================== Load Running script =========================================================
 source("sc_qc.R")
 source("sc_doublefinder.R")
 
-#==================================Read 10X data==================================
+#=========================================================== Read 10X data ===============================================================
 ##10X .h5 
 seurat_obj_1 <- Read10X_h5("seurat_obj_1.h5",use.names = T,unique.features = T)                                    
 seurat_obj_1 <- CreateSeuratObject(counts = seurat_obj_1, project = "seurat_obj_1", min.cells = 3, min.features = 200)
@@ -37,9 +32,9 @@ seurat_obj_2 <- CreateSeuratObject(counts = seurat_obj_2, project = "seurat_obj_
 ncol(seurat_obj_1)
 ncol(seurat_obj_2)
 
-#==================================Run sc_qc.R==================================
-seurat_obj_1_qc <- sc_qc(seurat_obj = seurat_obj_1, out_dir = "../03.Output/")
-seurat_obj_2_qc <- sc_qc(seurat_obj = seurat_obj_2, out_dir = "../03.Output/")
+#============================================================Run sc_qc.R==================================================================
+seurat_obj_1_qc <- sc_qc(seurat_obj = seurat_obj_1, out_dir = "./01.QC/")
+seurat_obj_2_qc <- sc_qc(seurat_obj = seurat_obj_2, out_dir = "./01.QC/")
 
 ##Process QC based on above result
 ###Eg.seurat_obj_1
@@ -50,20 +45,24 @@ seurat_obj_1_filtered <- subset(seurat_obj_1_qc,
                                 percent.RBC < 1)
 ncol(seurat_obj_1_filtered)
 p_seurat_obj_1_filtered <- VlnPlot(seurat_obj_1_filtered, 
-                                   features = c("nFeature_RNA", "nCount_RNA", "percent.mt","percent.ribo1","percent.ribo2","percent.RBC"), 
+                                   features = c("nFeature_RNA", "nCount_RNA", "percent.mt","percent.ribo1","percent.ribo2","percent_RBC"), 
                                    ncol = 3,pt.size = 0)
 p_seurat_obj_1_filtered
 ggsave("../03.Output/seurat_obj_1/seurat_obj_1_qc_mt_violin_filtered.pdf", plot = p_seurat_obj_1_filtered, width = 10, height = 8, dpi = 300)   #Head QC effect
 
-#==================================Run sc_doublefinder.R==================================
-seurat_obj_1_db <- sc_doublefinder(seurat_obj= seurat_obj_1_filtered ,max.dim = 40 , max.pcs=40, pN = 0.25, out_dir = "../03.Output/")
+#===========================================================Run sc_doublefinder.R==========================================================
+seurat_obj_1_db <- sc_doublefinder(seurat_obj= seurat_obj_1_filtered ,
+                                   max.dim = 40 , 
+                                   max.pcs=40, 
+                                   pN = 0.25, 
+                                   out_dir = "./01.QC/")
 
 
 #==================================Merge singledata==============================
 seurat_obj_merge <- merge(seurat_obj_1_db, y = c(seurat_obj_2_db, ...), add.cell.ids = c("seurat_obj_1", "seurat_obj_2",...), project = "seurat_obj_merge")
 ncol(seurat_obj_merge)
 
-#==================================Output==============================
+#=============================================================Output=========================================================================
 ##JoinLayers
 seurat_obj_merge[["RNA"]] <- JoinLayers(seurat_obj_merge[["RNA"]])
 ## rds
